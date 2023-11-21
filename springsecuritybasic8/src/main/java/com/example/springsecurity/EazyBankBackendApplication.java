137bdace470fa346b9e6d735303d4795ded9422b
DenisMurashko <dennn87@mail.ru>
2023-11-21 16:31:33 +0200

    Added 8 (Custom Filters) and 9 part without changes

---
+++ springsecuritybasic8/src/main/java/com/example/springsecurity/EazyBankBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity(debug = true)// Do not use in a production system!
+public class EazyBankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazyBankBackendApplication.class, args);
+    }
+
+}
