2135696c698421f0917291faf2a6fc2b879f284b
DenisMurashko <dennn87@mail.ru>
2024-01-07 18:19:51 +0200

    Added keycloak

---
+++ springsecuritybasic13/src/main/java/com/example/springsecurity/controller/BalanceController.java
@@ -0,0 +1,35 @@
+package com.example.springsecurity.controller;
+
+import com.example.springsecurity.model.AccountTransactions;
+import com.example.springsecurity.model.Customer;
+import com.example.springsecurity.repository.AccountTransactionsRepository;
+import com.example.springsecurity.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BalanceController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private AccountTransactionsRepository accountTransactionsRepository;
+
+    @GetMapping("/myBalance")
+    public List<AccountTransactions> getBalanceDetails(@RequestParam String email) {
+        List<Customer> customers = customerRepository.findByEmail(email);
+        if (customers != null && !customers.isEmpty()) {
+            List<AccountTransactions> accountTransactions = accountTransactionsRepository.
+                    findByCustomerIdOrderByTransactionDtDesc(customers.get(0).getId());
+            if (accountTransactions != null ) {
+                return accountTransactions;
+            }
+        }
+        return null;
+    }
+}
---
+++ springsecuritybasic13/src/main/java/com/example/springsecurity/controller/CardsController.java
@@ -0,0 +1,33 @@
+package com.example.springsecurity.controller;
+
+import com.example.springsecurity.model.Cards;
+import com.example.springsecurity.model.Customer;
+import com.example.springsecurity.repository.CardsRepository;
+import com.example.springsecurity.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CardsController {
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @GetMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestParam String email) {
+        List<Customer> customers = customerRepository.findByEmail(email);
+        if (customers != null && !customers.isEmpty()) {
+            List<Cards> cards = cardsRepository.findByCustomerId(customers.get(0).getId());
+            if (cards != null ) {
+                return cards;
+            }
+        }
+        return null;
+    }
+}
